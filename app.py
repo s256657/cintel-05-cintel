02b6b1884fbf7a8a959727ef320ed7feb5d79df2
Craig Wilcox <cwilcox5213@gmail.com>
2025-07-24 15:31:34 -0500

    changed links to proper repo

--- app.py
+++ app.py
@@ -104,12 +104,12 @@ with ui.sidebar(open="open"):
     ui.h6("Links:")
     ui.a(
         "GitHub Source",
-        href="https://github.com/denisecase/cintel-05-cintel",
+        href="https://https://github.com/s256657/cintel-05-cintel",
         target="_blank",
     )
     ui.a(
         "GitHub App",
-        href="https://denisecase.github.io/cintel-05-cintel/",
+        href="https://s256657.github.io/cintel-05-cintel/",
         target="_blank",
     )
     ui.a("PyShiny", href="https://shiny.posit.co/py/", target="_blank")
